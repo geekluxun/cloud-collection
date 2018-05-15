47533582084e3f36bc1f03194c6d6885e9597837
geekluxun <geekluxun@163.com>
2018-05-15 11:37:43 +0800

    提交

--- cloud-collection-web/src/main/java/com/geekluxun/ServletInitializer.java
+++ cloud-collection-web/src/main/java/com/geekluxun/ServletInitializer.java
@@ -1,7 +1,7 @@
 package com.geekluxun;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 /**
