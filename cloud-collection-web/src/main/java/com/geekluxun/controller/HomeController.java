451bf745d712662ed1f803a114e95a7daeabd7f5
geekluxun <geekluxun@163.com>
2018-05-15 09:55:42 +0800

    测试

---
+++ cloud-collection-web/src/main/java/com/geekluxun/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.geekluxun.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+/**
+ * Copyright,2018-2019,geekluxun Co.,Ltd.
+ * @Author: luxun
+ * @Date: 2018-05-15
+ * @Description: 
+ * @Others:
+ */
+
+@RestController
+public class HomeController {
+    
+    
+    @RequestMapping("test")
+    @ResponseBody
+    public Object helloWorld(){
+        Map response = new HashMap(10);
+        
+        response.put("code", "000000");
+        response.put("msg", "success");
+        return  response;
+    }
+}
